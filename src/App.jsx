334117e0c697a6823003445e243ed34310e631e3
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2025-04-12 21:26:41 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+import initialShopData  from './data';
+import MainPage from './components/pages/MainPage';
+import DetailPage from './components/pages/DetailPage';
+import './App.css';
 
 function App(){
-  return (
+
+  const [shopData, setshopData] = useState(initialShopData);
+
+  return (    
     
+    <>    
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -18,6 +27,13 @@ function App(){
         </Container>
       </Navbar>
     
+    
+      <Routes>
+        <Route path="/" element={ <MainPage shopData = {shopData}/> } />        
+        <Route path="/detail" element={<DetailPage shopData={shopData}/>} />    
+      </Routes>
+    
+    </>
   )
 }
 
