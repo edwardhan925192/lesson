e0c99b2ea133a18580216aaff4dd47218b1b6562
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2025-04-13 12:47:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
 import initialShopData  from './data';
 import MainPage from './components/pages/MainPage';
-import DetailPage from './components/pages/DetailPage';
+import DetailPage from './components/pages/DetailPages';
 import './App.css';
 
 function App(){
@@ -30,10 +30,23 @@ function App(){
     
       <Routes>
         <Route path="/" element={ <MainPage shopData = {shopData}/> } />        
-        <Route path="/detail" element={<DetailPage shopData={shopData}/>} />    
+        <Route path="/detail/:id" element={<DetailPage shopData={shopData}/>} />
+        <Route path="/event" element={ <About/> } >  
+          <Route path="one" element={ <div>첫 주문시 1,000원 할인</div> } />
+          <Route path="two" element={ <div>생일기념쿠폰</div> } />
+        </Route> 
       </Routes>
     
     </>
+  )
+}
+
+function About(){
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
   )
 }
 
