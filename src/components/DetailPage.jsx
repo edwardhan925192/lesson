63273f2ce3a0bd74558501b7734c11fac45b6004
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2025-04-14 15:38:41 +0900

    Update DetailPage.jsx

--- src/components/DetailPage.jsx
+++ src/components/DetailPage.jsx
@@ -1,16 +1,17 @@
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Tab } from 'bootstrap';
 
 function DetailPage(props){        
     
     let {id} = useParams(); // useParams --> {id : 1}
     let product = props.shopData[id];    
-
-    // alert 표시 여부를 관리할 state
+    
     const [showAlert, setShowAlert] = useState(true);
-    const [inputValue, setInputValue] = useState('');
-    const [hasError, setHasError] = useState(false); // 에러 상태 추가
-    const [count, setCount] = useState(0);
+    const [tab, setTab] = useState(0);
+    
 
     useEffect(() => {
         // 2초 후에 showAlert를 false로 변경
@@ -24,20 +25,7 @@ function DetailPage(props){
             clearTimeout(timer);
         }
     }, []);
-
-    // ---------------------------------------------
-    // inputValue를 감시하는 useEffect
-    // ---------------------------------------------    
-    useEffect(() => {
-        // 정규식을 사용해 숫자 포함 여부 확인
-        const hasNumber = /[0-9]/.test(inputValue);
-        
-        if (hasNumber) {
-            setHasError(true);
-        } else {
-            setHasError(false);
-        }
-    }, [inputValue]); // inputValue가 변경될 때마다 실행
+    
 
     return (
         <div className="container">
@@ -47,23 +35,10 @@ function DetailPage(props){
         <div className='alert alert-warning'>
             2초 이내 구매시 90% 할인합니다 ㅎㅎ. 
         </div>
-        )}
+        )}           
 
-        <input 
-        onChange={(e) => setInputValue(e.target.value)}  
-        placeholder="단어를 입력하세요" 
-        value={inputValue}
-        />
 
-        {/* 에러 메시지 - 단순하게 구현 */}
-        {hasError && (
-        <div style={{color: 'red', marginTop: '5px'}}>
-            숫자는 입력할 수 없습니다!
-        </div>
-        )}
-
-        <div className="row">
-        <button onClick={()=>{setCount(count+1)}}>카운트 버튼</button>
+        <div className="row">        
           <div className="col-md-6">
             <img src={`https://codingapple1.github.io/shop/shoes${Number(id) + 1}.jpg`} width="100%" />
           </div>
@@ -74,8 +49,28 @@ function DetailPage(props){
             <button className="btn btn-danger">주문하기</button> 
           </div>
         </div>
+
+        <Nav variant="tabs"  defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link eventKey="link0" onClick={()=>{setTab(0)}}>버튼0</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link1" onClick={()=>{setTab(1)}}>버튼1</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link2" onClick={()=>{setTab(2)}}>버튼2</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <TabContent tab={tab}/>
       </div> 
     )
   }
+
+const TabContent = (props) => {
+return(
+  [ <div>내용0</div>, <div>내용1</div>, <div>내용2</div> ][props.tab]
+)
+};
+
 
 export default DetailPage
